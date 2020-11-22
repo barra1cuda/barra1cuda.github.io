2cf1a924fa671a14d9123fc90a864f1013eb03aa
Hauke <hauke.jan@gmx.de>
2020-11-22 18:04:31 +0100

    improved scaling and fixed bugs

--- script.js
+++ script.js
@@ -2,111 +2,104 @@ var player = 1;
 var score1 = 0;
 var score2 = 0;
 var score3 = 0;
-var round = 1;
-var wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+var round = 0;
+var wins = [['00', '01', '02'], ['10', '11', '12'], ['20', '21', '22'], ['00', '10', '20'],
+ ['01', '11', '21'], ['02', '12', '22'], ['00', '11', '22'], ['20', '11', '02']];
 
-function my_log() {
-    console.log("Player: " + player);
-    console.log("Wins for player 1: " + score1);
-    console.log("Wins for player 2: " + score2);
-    console.log("Draws: " + score3);
-    console.log("Round: " + round);
+function myLog() {
+  console.log("Player: " + player);
+  console.log("Wins for player 1: " + score1);
+  console.log("Wins for player 2: " + score2);
+  console.log("Draws: " + score3);
+  console.log("Round: " + round);
 }
-        
-function reset_field() {
-    var i;
-    for (i = 0; i < 9; i++) {
-        document.getElementById("feld_" + i).value = "  ";
+
+function resetField() {
+  for (var i = 0; i <= 2; i++) {
+    for (var j = 0; j <= 2; j++) {
+      document.getElementById("feld_" + i + j).value = "  ";
     }
+  }
+  player = 1;
+  setStats();
 }
 
-function resetscore() {
-    player = 1;
-    score1 = 0;
-    score2 = 0;
-    score3 = 0;
-    document.getElementById('tabscore1').innerHTML = score1;
-    document.getElementById('tabscore2').innerHTML = score2;
+function resetStats() {
+  score1 = 0;
+  score2 = 0;
+  score3 = 0;
+  round = score1 + score2 + score3;
+  setStats();
 }
 
-function resetround() {
-    player = 1;
-    score1 = 0;
-    score2 = 0;
-    score3 = 0;
-    round = score1 + score2 + score3 + 1;
-    document.getElementById('round').innerHTML = round;
-}
-        
-function set_score() {
-    document.getElementById('tabscore1').innerHTML = score1;
-    document.getElementById('tabscore2').innerHTML = score2;
-    document.getElementById('round').innerHTML = round;
+function setStats() {
+  document.getElementById('pScore1').innerHTML = score1;
+  document.getElementById('pScore2').innerHTML = score2;
+  document.getElementById('pRound').innerHTML = round;
 }
 
 function check_for_win() {
-    for (my_list of wins) {
-        if (document.getElementById("feld_" + my_list[0]).value === "X") {
-            if (document.getElementById("feld_" + my_list[1]).value === "X") {
-                if (document.getElementById("feld_" + my_list[2]).value === "X") {
-                    alert("Player 1 wins !");
-                    score1++;
-                    round++;
-                    player = 1;
-                    reset_field();
-                }
-            }
+  for (my_list of wins) {
+    if (document.getElementById("feld_" + my_list[0]).value === "X") {
+      if (document.getElementById("feld_" + my_list[1]).value === "X") {
+        if (document.getElementById("feld_" + my_list[2]).value === "X") {
+          alert("Player 1 wins !");
+          score1++;
+          round++;
+          resetField();
         }
+      }
     }
-    
-    for (my_list of wins) {
-        if (document.getElementById("feld_" + my_list[0]).value === "O") {
-            if (document.getElementById("feld_" + my_list[1]).value === "O") {
-                if (document.getElementById("feld_" + my_list[2]).value === "O") {
-                    alert("Player 2 wins !");
-                    score2++;
-                    round++;
-                    player = 1;
-                    reset_field();
-                }
-            }
-        }
-    }
+  }
 
-    var draw_game = true, i;
-    for (i = 0; i <= 8; i++) {
-        if (document.getElementById('feld_' + i).value === "  ") {
-            draw_game = false;
-        }  
+  for (my_list of wins) {
+    if (document.getElementById("feld_" + my_list[0]).value === "O") {
+      if (document.getElementById("feld_" + my_list[1]).value === "O") {
+        if (document.getElementById("feld_" + my_list[2]).value === "O") {
+          alert("Player 2 wins !");
+          score2++;
+          round++;
+          resetField();
+        }
+      }
     }
-    if (draw_game) {
-        alert("Draw!");
-        score3++;
-        round++;
-        player = 1;
-        reset_field();
+  }
+
+  var draw_game = true;
+  for (var i = 0; i <= 2; i++) {
+    for (var j = 0; j <= 2; j++) {
+      if (document.getElementById('feld_' + i + j).value === "  ") {
+      draw_game = false;
+      }
+    }
+  }
+  if (draw_game) {
+    alert("Draw!");
+    score3++;
+    round++;
+    resetField();
     }
 }
 
 function run(num) {
-    my_log();
-    if (player === 1 && document.getElementById("feld_" + num).value === "  ") {
-        document.getElementById("feld_" + num).value = "X";
-        player = 2;
-    } else if (player === 2 && document.getElementById("feld_" + num).value === "  ") {
-        document.getElementById("feld_" + num).value = "O";
-        player = 1;
-    }
-    check_for_win();
-    set_score();
+  myLog();
+  if (player === 1 && document.getElementById("feld_" + num).value === "  ") {
+    document.getElementById("feld_" + num).value = "X";
+    player = 2;
+  } else if (player === 2 && document.getElementById("feld_" + num).value === "  ") {
+    document.getElementById("feld_" + num).value = "O";
+    player = 1;
+  }
+  check_for_win();
+  setStats();
+  
 }
 
-function position_buttons() {
-    for (var i = 0; i <= 2; i++) {
-        for (var j = 0; j <= 2; j++) {
-            document.getElementById('feld_' + (i + j * 3)).style.top = 1 + 33 * i + "%";
-            document.getElementById('feld_' + (i + j * 3)).style.left = 1 + 33 * j + "%";
-        }
+function positionButtons() {
+  for (var i = 0; i <= 2; i++) {
+    for (var j = 0; j <= 2; j++) {
+      document.getElementById('feld_' + i + j).style.top = 1 + 33 * i + "%";
+      document.getElementById('feld_' + i + j).style.left = 1 + 33 * j + "%";
     }
-
+  }
 }
